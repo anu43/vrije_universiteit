2ad48262c860a8f429d237cc4b971a2e53d5a3e7
anu43 <56151687+anu43@users.noreply.github.com>
2020-05-15 19:34:48 +0300

    added specification of bool variables in xgbRanker algorithm

--- dmt/assignment2/training.py
+++ dmt/assignment2/training.py
@@ -6,6 +6,15 @@ from time import time
 from sklearn.model_selection import train_test_split
 
 
+categoricalCols = [
+    'srch_id', 'site_id', 'visitor_location_country_id',
+    'prop_country_id', 'prop_id', 'prop_brand_bool',
+    'promotion_flag', 'srch_destination_id',
+    'random_bool', 'with_family', 'is_Fall',
+    'is_Spring', 'is_Summer', 'is_Winter'
+]
+
+
 def train_lgbm_model(train, test):
     '''
     Returns the tested dataset after the training
@@ -95,11 +104,11 @@ def train_xgboost_model(train, test):
     dtrain = xgb.DMatrix(X_train, label=y_train)
     # Create XGBoost model
     parameters = {
-        'max_depth': 7,
+        'max_depth': 10,
         'eta': 1,
         'silent': 1,
         'objective': 'reg:squarederror',
-        'eval_metric': 'auc',
+        'eval_metric': 'ndcg',
         'learning_rate': .05
     }
 
@@ -127,6 +136,67 @@ def train_xgboost_model(train, test):
     return test
 
 
+def train_xgbRanker_model(train, test):
+    
+    # Define bool variables
+    boolVars = [
+            'prop_brand_bool',
+            'promotion_flag',
+            'srch_saturday_night_bool',
+            'random_bool'
+        ]
+    # Change categorical columns' type
+    train[boolVars] = train[boolVars].astype('bool')
+    test[boolVars] = test[boolVars].astype('bool')
+    # Prepare X and y
+    target = 'target'  # Set column name for y
+    # Receive all columns except target column for X
+    X_train = train.loc[:, train.columns != target]
+    # Get the target column only
+    y_train = train.loc[:, target]
+
+    # Split training data as training and validation
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train,
+                                                      test_size=0.2,
+                                                      random_state=43)
+
+    # Set the queries for LGBM
+    query_train = [X_train.shape[0]]
+    query_val = [X_val.shape[0]]
+
+    params = {
+        'objective': 'rank:ndcg',
+        'eval_metric': 'ndcg',
+        'learning_rate': 0.1,
+        'gamma': 1.0,
+        'min_child_weight': 0.1,
+        'max_depth': 10,
+        'n_estimators': 10
+    }
+
+    ranker = xgb.sklearn.XGBRanker(**params)
+
+    # Set start time
+    start = time()
+    # Fit the model
+    ranker.fit(X_train, y_train, group=query_train,
+               eval_set=[(X_val, y_val)], eval_group=[query_val],
+               early_stopping_rounds=50, verbose=False)
+    # Set stop time
+    stop = time()
+    # Print training time
+    print('XGB Ranker training time', (stop - start) / 60, 'mins')
+
+    # Reindex columns for prediction
+    cols = list(ranker.get_booster().feature_names)
+    test = test.reindex(cols, axis=1)
+    # Predict
+    preds = ranker.predict(test)
+    # Put the predictions to the test frame
+    test["ranking_rates"] = preds
+    return test
+
+
 # Import training/test sets
 train = pd.read_csv('../../../../data/training.csv')
 test = pd.read_csv('../../../../data/test.csv')
@@ -135,7 +205,10 @@ test = pd.read_csv('../../../../data/test.csv')
 test = train_lgbm_model(train, test)
 
 # Train XGBoost model
-xg = train_xgboost_model(train, test)
+test = train_xgboost_model(train, test)
+
+# Train XGBoostRanker model
+test = train_xgbRanker_model(train, test)
 
 # Write the result to csv
 test.to_csv('../../../../data/predicted_test.csv', index=False)
