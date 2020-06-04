c0bf807c8c98ff0e17646ce22c85ae8d8773c5e8
anu43 <56151687+anu43@users.noreply.github.com>
2020-06-04 22:29:40 +0300

    added a function which visualizes gyroscope x, y, z, and absolute rates

--- ml4qs/chapter2coding1.py
+++ ml4qs/chapter2coding1.py
@@ -8,6 +8,7 @@ Created on Thu Jun  4 21:11:29 2020
 
 
 # Import libraries
+import matplotlib.pyplot as plt
 import pandas as pd
 
 # Define main path for importing
@@ -21,6 +22,30 @@ file_names = {
     'Pressure.csv': None
 }
 
+
+def visualize_gyroscope_data(file_name: str):
+    'Visualizes the gyroscope data with several different granularities'
+    # Set data
+    data = file_names[file_name]
+    # Create axes
+    fig, axs = plt.subplots(4, figsize=(10, 15))
+
+    # Plot features
+    axs[0].plot(data.index, data.iloc[:, 0])  # X axis
+    axs[1].plot(data.index, data.iloc[:, 1], 'tab:orange')  # Y axis
+    axs[2].plot(data.index, data.iloc[:, 2], 'tab:green')  # Z axis
+    axs[3].plot(data.index, data.iloc[:, 3], 'tab:red')  # Absolute value
+
+    # Set titles
+    axs[0].set_title('X Axis')
+    axs[1].set_title('Y Axis')
+    axs[2].set_title('Z Axis')
+    axs[3].set_title('Absolute Rate')
+
+    # Show plot
+    plt.show()
+
+
 # Import data from given path
 for file_name in file_names:
     # Import csv and put it into dictionary
@@ -32,3 +57,7 @@ for file_name in file_names:
     file_names[file_name].drop('Time (s)', axis=1, inplace=True)
     # Set new converted time as index
     file_names[file_name].set_index('time', inplace=True)
+
+# Visualize frames
+visualize_gyroscope_data('Gyroscope_rotation_rate.csv')  # Gyroscope data
+visualize_gyroscope_data('Gyroscope_rotation_rate2.csv')  # Gyroscope data
