2b93371afcc7ba6142eec8506bf81633d2033e13
anu43 <56151687+anu43@users.noreply.github.com>
2020-06-04 21:18:24 +0300

    added libraries

--- ml4qs/chapter2coding1.py
+++ ml4qs/chapter2coding1.py
@@ -1,1 +1,1 @@
-#!/usr/bin/env python3# -*- coding: utf-8 -*-"""Created on Thu Jun  4 21:11:29 2020@author: anu"""
+#!/usr/bin/env python3# -*- coding: utf-8 -*-"""Created on Thu Jun  4 21:11:29 2020@author: anu"""# Import librariesimport pandas as pd
