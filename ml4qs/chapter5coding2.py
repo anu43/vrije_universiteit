3ef99d098e58f4c990cdac9c4246aa8016fe0d83
anu43 <56151687+anu43@users.noreply.github.com>
2020-06-09 00:16:44 +0300

    plot 3D

--- ml4qs/chapter5coding2.py
+++ ml4qs/chapter5coding2.py
@@ -5,3 +5,13 @@ import pandas as pd
 # Import dataset
 df = pd.read_csv('../data/ml4qs/created_by_phyphox/Gyroscope_rotation_rate2.csv')
 df = df.set_index(pd.to_timedelta(df.time, 'ms'))
+
+# Set figure and axes
+fig = plt.figure()
+ax = plt.axes(projection="3d")
+
+# Plot 3D
+ax.scatter3D(df.gyro_x, df.gyro_y, df.gyro_z, c=df.gyro_z, cmap='hsv')
+
+# Show plot
+plt.show()
