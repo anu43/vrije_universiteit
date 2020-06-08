c89c89d0a108c9bca01086efb04ad621696a4ed7
anu43 <56151687+anu43@users.noreply.github.com>
2020-06-09 00:08:17 +0300

    import dataset

--- ml4qs/chapter5coding2.py
+++ ml4qs/chapter5coding2.py
@@ -1,3 +1,7 @@
 # Import libraries
 import matplotlib.pyplot as plt
 import pandas as pd
+
+# Import dataset
+df = pd.read_csv('../data/ml4qs/created_by_phyphox/Gyroscope_rotation_rate2.csv')
+df = df.set_index(pd.to_timedelta(df.time, 'ms'))
