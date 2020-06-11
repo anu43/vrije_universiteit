4ff4295609296a52ded6a40e2ec0d68ecfcbfed1
anu43 <56151687+anu43@users.noreply.github.com>
2020-06-11 13:32:39 +0300

    imported dataset, converted time feature to timedelta, and set it as index

--- ml4qs/chapter7coding3.py
+++ ml4qs/chapter7coding3.py
@@ -1,2 +1,9 @@
 # Import libraries
 import pandas as pd
+
+# Import data
+df = pd.read_csv('../data/ml4qs/created_by_phyphox/Gyroscope_rotation_rate2.csv')
+# Convert time to timedelta
+df.time = pd.to_timedelta(df.time, 'S')
+# Set time feature as index
+df.set_index('time', inplace=True)
