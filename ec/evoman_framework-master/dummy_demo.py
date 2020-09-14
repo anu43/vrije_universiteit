ef2454bca00386c97816252c82ca01d5fad69984
anu43 <56151687+anu43@users.noreply.github.com>
2020-09-14 17:31:30 +0300

    added multithread setup for running evoman analysis. NOT WORKING RN

--- ec/evoman_framework-master/dummy_demo.py
+++ ec/evoman_framework-master/dummy_demo.py
@@ -5,15 +5,41 @@
 ################################
 
 # imports framework
-from environment import Environment
+from concurrent.futures import ProcessPoolExecutor
+import numpy as np
 import sys
 import os
 sys.path.insert(0, 'evoman')
+from demo_controller import player_controller
+from environment import Environment
 
 experiment_name = 'taskI'
 if not os.path.exists(experiment_name):
     os.makedirs(experiment_name)
 
-# initializes environment with ai player using random controller, playing against static enemy
-env = Environment(experiment_name=experiment_name)
-env.play()
+
+# def helper(enemy):
+#     # Update the number of neurons for this specific example
+#     n_hidden_neurons = 0
+#     # Create the environment
+#     env = Environment(experiment_name=experiment_name,
+#                       playermode="ai",
+#                       enemies=[enemy],
+#                       player_controller=player_controller(n_hidden_neurons),
+#                       speed="fastest",
+#                       enemymode="static",
+#                       level=2)
+#     # Logging
+#     # Load specialist controller
+#     sol = np.loadtxt('solutions_demo/demo_'+str(enemy)+'.txt')
+#     print('\n LOADING SAVED SPECIALIST SOLUTION FOR ENEMY '+str(enemy)+' \n')
+#     # Play the given env
+#     env.play(sol)
+
+
+# with ProcessPoolExecutor() as executor:
+#     enemies = [1, 2, 3]
+#     results = executor.map(helper, enemies)
+
+# for result in results:
+#     print(result)
