40658e8c4aedc0e8063e9c10b83ff8250e023caf
anu43 <56151687+anu43@users.noreply.github.com>
2020-09-16 12:19:48 +0300

    added multithread running setup but commented. NOT WORKING RN

--- ec/evoman_framework-master/taskI_demo.py
+++ ec/evoman_framework-master/taskI_demo.py
@@ -13,3 +13,29 @@ sys.path.insert(0, 'evoman')
 experiment_name = 'taskI'
 if not os.path.exists(experiment_name):
     os.makedirs(experiment_name)
+
+# def helper(enemy):
+#     # Update the number of neurons for this specific example
+#     n_hidden_neurons = 0
+#     # Create the environment
+#     env = Environment(experiment_name=experiment_name,
+#                       playermode="ai",
+#                       enemies=[enemy],
+#                       player_controller=player_controller(n_hidden_neurons),
+#                       speed="fastest",
+#                       enemymode="static",
+#                       level=2)
+#     # Logging
+#     # Load specialist controller
+#     sol = np.loadtxt('solutions_demo/demo_'+str(enemy)+'.txt')
+#     print('\n LOADING SAVED SPECIALIST SOLUTION FOR ENEMY '+str(enemy)+' \n')
+#     # Play the given env
+#     env.play(sol)
+
+
+# with ProcessPoolExecutor() as executor:
+#     enemies = [1, 2, 3]
+#     results = executor.map(helper, enemies)
+
+# for result in results:
+#     print(result)
