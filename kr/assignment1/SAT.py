0af193ad9e1aa0e5764eb146cd3f6da92ef6329c
anu43 <56151687+anu43@users.noreply.github.com>
2020-09-17 00:06:31 +0300

    deleted unprocessed attribute from dpll, init modify_unit_clauses, and added pure_literal checker

--- kr/assignment1/SAT.py
+++ kr/assignment1/SAT.py
@@ -3,10 +3,38 @@ import os
 import sys
 import time
 import mxklabs.dimacs
+from collections import Counter
 
 
-def dpll(clauses: list, init_clauses: dict,
-         unprocessed: set):
+def modify_unit_clause(clauses: list, unit: int):
+    return clauses
+
+
+def pure_literal(clauses: list):
+    # Declare the counter of literals
+    counter: Counter = Counter([literal for clause in clauses for literal in clause])
+    # Declare lists
+    assignment: list = list()  # assignment
+    pure_literals: list = list()  # pure literals
+    # Iterate through counter to check whether there is a pure literal
+    for literal, occur in counter.items():
+        # If polar literal does not occur in counter
+        if -literal not in counter:
+            # Append the literal as pure literal
+            pure_literals.append(literal)
+    # Iterate through pure literals to unit check
+    for pure in pure_literals:
+        # Modify unit clauses
+        clauses = modify_unit_clause(clauses, pure)
+    # Assign pure literals
+    assignment += pure_literals
+    # Return clauses and assignment
+    print(clauses)
+    print(assignment)
+    return clauses, assignment
+
+
+def dpll(clauses: list, init_clauses: dict):
     pass
 
 
@@ -23,4 +51,4 @@ if __name__ == '__main__':
     }), None)
     # Declare solver
     if sys.argv[1] == '-S1':
-        solution = dpll()
+        solution = dpll(clauses, init_clauses)
