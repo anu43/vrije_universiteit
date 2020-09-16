e84bae99dfeb9d963dec57460344aa01bac44ec4
anu43 <56151687+anu43@users.noreply.github.com>
2020-09-16 23:43:22 +0300

    read CNF file and init basic dpll function

--- kr/assignment1/SAT.py
+++ kr/assignment1/SAT.py
@@ -3,3 +3,24 @@ import os
 import sys
 import time
 import mxklabs.dimacs
+
+
+def dpll(clauses: list, init_clauses: dict,
+         unprocessed: set):
+    pass
+
+
+if __name__ == '__main__':
+    # Declare file name
+    file_name = sys.argv[2]
+    # Read file and get the clauses
+    clauses: list = mxklabs.dimacs.read(file_name).clauses
+    # Convert DIMACS format to set
+    clauses: list = list(map(lambda clause: {*clause}, clauses))
+    # Initiliaze literals with None
+    init_clauses: set = dict.fromkeys(sorted({
+        abs(literal) for clause in clauses for literal in clause
+    }), None)
+    # Declare solver
+    if sys.argv[1] == '-S1':
+        solution = dpll()
