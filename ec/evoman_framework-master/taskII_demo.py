910e44cc8ad21f6057c4b826b450ae9e6d3b4274
anu43 <56151687+anu43@users.noreply.github.com>
2020-10-01 22:01:38 +0300

    init taskII

---
+++ ec/evoman_framework-master/taskII_demo.py
@@ -0,0 +1,14 @@
+# imports framework
+import sys
+sys.path.insert(0, 'evoman')
+from environment import Environment
+from demo_controller import player_controller
+
+# imports other libs
+import time
+import numpy as np
+from math import fabs,sqrt
+import glob, os
+
+# Disable the game screen
+os.environ["SDL_VIDEODRIVER"] = "dummy"
