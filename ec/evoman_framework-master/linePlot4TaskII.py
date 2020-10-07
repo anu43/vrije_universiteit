8494ecefa0561b6b55b1cec22bbe4ade688783dc
anu43 <56151687+anu43@users.noreply.github.com>
2020-10-07 20:42:16 +0300

    line plotting from the experimental results

---
+++ ec/evoman_framework-master/linePlot4TaskII.py
@@ -0,0 +1,47 @@
+# Import libraries
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Declare array of runs
+runs = np.arange(1, 11)
+
+# Import records from EA1-GROUP1
+# Group 1
+group1_means = np.loadtxt('ea1-group1/ea1_means')
+group1_stdOfmeans = np.loadtxt('ea1-group1/ea1_stdOfmeans')
+group1_maxs = np.loadtxt('ea1-group1/ea1_maxs')
+group1_stdOfmaxs = np.loadtxt('ea1-group1/ea1_stdOfmaxs')
+# Group 2
+group2_means = np.loadtxt('ea1-group2/ea1_means')
+group2_stdOfmeans = np.loadtxt('ea1-group2/ea1_stdOfmeans')
+group2_maxs = np.loadtxt('ea1-group2/ea1_maxs')
+group2_stdOfmaxs = np.loadtxt('ea1-group2/ea1_stdOfmaxs')
+
+# Create figure and axes for the subplots
+axs = plt.figure(figsize=(10, 8)).subplots(4, 1)
+
+# Plot average mean across all runs
+axs[0].set_title('Average mean')
+axs[0].plot(runs, group1_means, label='EA1-GROUP1')
+axs[0].plot(runs, group2_means, label='EA1-GROUP2')
+axs[0].legend(loc='upper right')
+# Plot the std of average mean across all runs
+axs[1].set_title('STD of average mean')
+axs[1].plot(runs, group1_stdOfmeans, label='EA1-GROUP1')
+axs[1].plot(runs, group2_stdOfmeans, label='EA1-GROUP2')
+axs[1].legend(loc='upper right')
+# Plot average max across all runs
+axs[2].set_title('Average max')
+axs[2].plot(runs, group1_maxs, label='EA1-GROUP1')
+axs[2].plot(runs, group2_maxs, label='EA1-GROUP2')
+axs[2].legend(loc='upper right')
+# Plot the std of average max across all runs
+axs[3].set_title('STD of average max')
+axs[3].plot(runs, group1_stdOfmaxs, label='EA1-GROUP1')
+axs[3].plot(runs, group2_stdOfmaxs, label='EA1-GROUP2')
+axs[3].legend(loc='upper right')
+
+# Tight layout
+plt.tight_layout()
+# Save figure
+plt.savefig('linePlot4TaskII_EA1')
